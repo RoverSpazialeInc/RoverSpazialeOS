ede3a5d465fc66f8785547dbe0c9efaba892cad5
Tyusibo <a.tudino@studenti.unisa.it>
2026-02-09 14:39:59 +0100

    stampa ogni 100

--- Board1/Core/Src/app_freertos.c
+++ Board1/Core/Src/app_freertos.c
@@ -427,7 +427,7 @@ void StartSupervisor(void *argument)
 
 		//printMotorSpeeds(&Board1_U.speed);
 
-		if (cycle_count >= 1) { // Approx 2 seconds (50ms * 40)
+		if (cycle_count >= 100) { // Approx 2 seconds (50ms * 40)
 			cycle_count = 0;
 			printGlobalState(&Board1_Y.board1GlobalState);
 			printDecision(&Board1_Y.board1Decision);
